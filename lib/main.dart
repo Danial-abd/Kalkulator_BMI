be4261889c4935b97d3ce46bc980c8744af1a5b4
Danial-abd <danial.aburrahman@gmail.com>
2021-10-30 22:20:09 +0800

    BMICalculator

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'input_pages.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xff0a0e21),
+        scaffoldBackgroundColor: Color(0xff0a0e21),
+      ),
+    );
+  }
+}
